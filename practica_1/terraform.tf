5a0509a2bd691ad43f8e2a15779aa6762c34abb9
Luis Alvarez <d0441@globaluniversity.edu.mx>
2024-11-14 18:54:23 -0600

    Adding practica1

---
+++ practica_1/terraform.tf
@@ -0,0 +1,26 @@
+resource "local_file" "foo" {
+  content  = "foo!"
+  filename = "${path.module}/foo.bar"
+}
+
+resource "local_file" "miarchivo"{
+  count=5 //Count nos permite saber cuantas veces va repetir una ejecucion
+  content  = "este es mi archivo"
+  filename = "${path.module}/miarchivo-${random_string.random[count.index].id}.txt"
+}
+
+//Random es parte de otro provider, necesitamos ejecutar terraform init 
+resource "random_string" "random" {
+  count = 5//Count nos permite saber cuantas veces va repetir una ejecucion
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
+
+/*
+Comandos a ejecutar
+terraform init 
+terraform plan
+terraform apply
+terraform destroy
+*/
